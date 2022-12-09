6415a341c9b2515055c519baaed6870981dad317
ynainggolan <yhpnainggolan21@gmail.com>
2022-12-08 21:52:19 -0500

    made employess container

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -1,3 +1,8 @@
 // Barrel file for container components (make it easier to import to App.js)
+
+export { default as AllEmployeesContainer } from "./AllEmployeesContainer";
+export { default as EmployeeContainer } from "./EmployeeContainer";
 export { default as AllTasksContiner } from "./AllTasksContainer"
 export { default as TaskContainer } from "./TaskContainer"
+
+
