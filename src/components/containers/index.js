5485448bce189f4ba275eff56f5d5dc1f675e331
ynainggolan <yhpnainggolan21@gmail.com>
2022-12-08 20:06:13 -0500

    made Tasks view

---
+++ src/components/containers/index.js
@@ -0,0 +1,3 @@
+// Barrel file for container components (make it easier to import to App.js)
+export { default as AllTasksContiner } from "./AllTasksContainer"
+export { default as TaskContainer } from "./TaskContainer"
