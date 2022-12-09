e168818a17f530b248bde432eb91b35003cacb33
ynainggolan <yhpnainggolan21@gmail.com>
2022-12-08 21:53:50 -0500

    made homepage containers

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -1,5 +1,5 @@
 // Barrel file for container components (make it easier to import to App.js)
-
+export { default as HomePageContainer } from "./HomePageContainer";
 export { default as AllEmployeesContainer } from "./AllEmployeesContainer";
 export { default as EmployeeContainer } from "./EmployeeContainer";
 export { default as AllTasksContiner } from "./AllTasksContainer"
