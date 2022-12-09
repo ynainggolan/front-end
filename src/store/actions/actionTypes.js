0549f2235fd864c06347012de3c56d78139ce6f4
ynainggolan <yhpnainggolan21@gmail.com>
2022-12-08 20:58:23 -0500

    made actions header file

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,16 @@
+// ACTION TYPES;
+
+//All Employee
+export const FETCH_ALL_EMPLOYEE = "FETCH_ALL_EMPLOYEE";
+
+//Single Employee
+export const FETCH_EMPLOYEE = "FETCH_EMPLOYEE";
+
+//All Task
+export const FETCH_ALL_TASKSS = "FETCH_ALL_TASKS";
+export const ADD_TASK = "ADD_TASKS";
+export const DELETE_TASK = "DELETE_TASK";
+export const EDIT_TASK = "EDIT_TASK";
+
+//Single Task
+export const FETCH_TASK = "FETCH_TASK";
