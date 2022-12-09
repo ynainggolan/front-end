28fc5e042190d205c6ebe1d4909f0bdf8dcc7854
ynainggolan <yhpnainggolan21@gmail.com>
2022-12-09 00:19:42 -0500

    added routes to containers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//Router
+import { Switch, Route } from "react-router-dom";
+//Components
+import {
+  HomePageContainer,
+  EmployeeContainer,
+  TaskContainer,
+  AllEmployeesContainer,
+  AllTasksContainer
+} from './components/containers';
+
+// if you create separate components for adding/editing 
+// a student or instructor, make sure you add routes to those
+// components here
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={HomePageContainer} />
+        <Route exact path="/employees" component={AllEmployeesContainer} />
+        <Route exact path="/employee/:id" component={EmployeeContainer} />
+        <Route exact path="/tasks" component={AllTasksContainer} />
+        <Route exact path="/Task/:id" component={TaskContainer} />
+
+      </Switch>        
     </div>
   );
 }
