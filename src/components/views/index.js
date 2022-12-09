784f11e25f5c9f16ef9a3897a97dc979b8bac32a
ynainggolan <yhpnainggolan21@gmail.com>
2022-12-09 00:12:41 -0500

    made paths to Employees and home page

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -1,3 +1,6 @@
 // Barrel file for presentational components, which will be used to compose our smart containers;
 export { default as AllTasksView } from './AllTasksView';
 export { default as TaskView } from './TaskView';
+export { default as AllEmployeesView } from './AllEmployeesView';
+export { default as EmployeeView } from './EmployeeView';
+export { default as HomePageView } from './HomePageView';
