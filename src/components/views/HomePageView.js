54c633edde89242fd29e67323c30f20e56a13224
ynainggolan <yhpnainggolan21@gmail.com>
2022-12-21 15:13:39 -0500

    fixed link/route to employees and tasks

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -6,7 +6,7 @@ const HomePageView = () => {
   return (
     <div>
       <h6>Final Project</h6>
-      <Link to={'/employees'} > All Employee </Link>
+      <Link to={'/employees'} > All Employees </Link>
       <Link to={'/tasks'} > All Tasks </Link>
       
     </div>
