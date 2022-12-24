1321c1184a27020b17241b035bc44566857bf378
ynainggolan <yhpnainggolan21@gmail.com>
2022-12-24 13:23:27 -0500

    made employee's view for new employee

---
+++ src/components/views/NewEmployeeView.js
@@ -0,0 +1,41 @@
+const NewEmployeeView = (props) => {
+    const {handleChange, handleSubmit, error } = props;
+  
+    return (
+      <div className="root">
+        <div className="formContainer">
+          <div className="formDescription">
+            <h2 style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
+              New Employee
+            </h2>
+          </div>
+          <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
+            <label style= {{color:'#11153e', fontWeight: 'bold'}}>First Name: </label>
+            <input type="text" name="firstName" onChange ={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+  
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>Last Name: </label>
+            <input type="text" name="lastName" onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+  
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>taskId: </label>
+            <input type="text" name="taskId" onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+  
+            <button type="submit">
+              Submit
+            </button>
+            <br/>
+            <br/>
+          </form>
+          {error!=="" && <p>{error}</p>}
+          </div>
+        </div>
+      
+    )
+  }
+  
+  export default NewEmployeeView;
